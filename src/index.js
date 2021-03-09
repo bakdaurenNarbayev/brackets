e352ba6cc355db51730979ac9d43859ee565b6e0
bakdaurenNarbayev <nbakdauren@gmail.com>
2021-03-09 22:13:59 +0600

    fix: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length < 2) return false;
+  let dict = {};
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    dict[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  st = [];
+  for(let i = 0; i < str.length; i++) {
+    st.push(str[i]);
+    if(st.length > 1 && dict[st[st.length - 2]] === st[st.length - 1]) {
+      st.pop();
+      st.pop();
+    }
+  }
+  return st.length === 0;
 }
